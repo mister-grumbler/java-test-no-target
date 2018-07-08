91a18de20ffbce02acad94c5e54b6e8cb1e65017
mister-grumbler <avmeklenko@gmail.com>
2018-07-08 03:34:00 +0300

    Configuration of data source was corrected. DB init script was updated.

--- seminars/src/main/resources/data.sql
+++ seminars/src/main/resources/data.sql
@@ -2,6 +2,6 @@ insert into app_professor(address,name,phone,salary) values("London","Winston Ch
 insert into app_professor(address,name,phone,salary) values("London","Stephen Hawking","+4437216498","2000");
 insert into app_student(address,email,marksaverage,marksbook,name,phone) values("34 Ridgmount Gardens, London, UK","bob.marley@gmail.com","0","1212123434","Bob Marley","+4412312312");
 insert into app_student(address,email,marksaverage,marksbook,name,phone) values("4 Central Square, London, UK","freddy.mercury@gmail.com","0","1032023421","Freddy Mercury","+4476305634");
-insert into app_seminar(name,number,fee) values("Mathematics","01","100");
-insert into app_seminar(name,number,fee) values("Physics","02","100");
-insert into app_seminar(name,number,fee) values("Social Communications","03","100");
+insert into app_seminar(professor_id,name,number,fee) values("1","Mathematics","01","100");
+insert into app_seminar(professor_id,name,number,fee) values("1","Physics","02","100");
+insert into app_seminar(professor_id,name,number,fee) values("2","Social Communications","03","100");
