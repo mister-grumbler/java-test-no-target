08180c3315e1cb98e80589d6e666e188c642a7cc
mister-grumbler <avmeklenko@gmail.com>
2018-08-22 15:53:58 +0300

    hashCode(), equals() and toString() autogenerated methods were added.

--- seminars/src/main/java/cf/jtarget/seminars/serializer/RootHolder.java
+++ seminars/src/main/java/cf/jtarget/seminars/serializer/RootHolder.java
@@ -23,7 +23,12 @@ public class RootHolder {
 	private List<Student> students;
 
 	/**
+	 * Constructor of new RootHolder object using exact data for fields
 	 * 
+	 * @param professors
+	 * @param progresses
+	 * @param seminars
+	 * @param students
 	 */
 	public RootHolder(List<Professor> professors, List<Progress> progresses, List<Seminar> seminars,
 			List<Student> students) {
@@ -35,7 +40,7 @@ public class RootHolder {
 	}
 
 	/**
-	 * 
+	 * Constructor of new RootHolder object with empty lists
 	 */
 	public RootHolder() {
 		super();
@@ -103,6 +108,70 @@ public class RootHolder {
 	 */
 	public void setStudents(List<Student> students) {
 		this.students = students;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((professors == null) ? 0 : professors.hashCode());
+		result = prime * result + ((progresses == null) ? 0 : progresses.hashCode());
+		result = prime * result + ((seminars == null) ? 0 : seminars.hashCode());
+		result = prime * result + ((students == null) ? 0 : students.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof RootHolder))
+			return false;
+		RootHolder other = (RootHolder) obj;
+		if (professors == null) {
+			if (other.professors != null)
+				return false;
+		} else if (!professors.equals(other.professors))
+			return false;
+		if (progresses == null) {
+			if (other.progresses != null)
+				return false;
+		} else if (!progresses.equals(other.progresses))
+			return false;
+		if (seminars == null) {
+			if (other.seminars != null)
+				return false;
+		} else if (!seminars.equals(other.seminars))
+			return false;
+		if (students == null) {
+			if (other.students != null)
+				return false;
+		} else if (!students.equals(other.students))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "RootHolder [professors=" + professors + ", progresses=" + progresses + ", seminars=" + seminars
+				+ ", students=" + students + "]";
 	}
 
 }
