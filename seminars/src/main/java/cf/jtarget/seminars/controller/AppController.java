daaec65d11cf9800a26c8411f4bd3e7ac39a8be1
mister-grumbler <avmeklenko@gmail.com>
2018-08-29 21:40:49 +0300

    Request mapping for partial templates was added.

--- seminars/src/main/java/cf/jtarget/seminars/controller/AppController.java
+++ seminars/src/main/java/cf/jtarget/seminars/controller/AppController.java
@@ -5,6 +5,7 @@ package cf.jtarget.seminars.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,9 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController {
 
+	private static final String PAGES = "backup seminar professor student";
 	@RequestMapping("/")
-	String home(ModelMap modal) {
-		modal.addAttribute("title", "Seminars Management");
+	String home(ModelMap model) {
+		model.addAttribute("title", "Seminars Management");
 		return "index";
+	}
+	@RequestMapping("/partials/{page}")
+	String partialHandler(@PathVariable("page") final String page) {
+		if (!PAGES.contains(page.toLowerCase())) {
+			return "seminar";
+		}
+		return page;
 	}
 }
