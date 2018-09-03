d3ffaea6d3bbf0b080020e52578d69653789a04a
mister-grumbler <avmeklenko@gmail.com>
2018-09-03 04:20:26 +0300

    service renamed to backupService, date stamp on/off function was added.

--- seminars/src/main/resources/static/js/app/BackupController.js
+++ seminars/src/main/resources/static/js/app/BackupController.js
@@ -14,9 +14,10 @@ angular.module('seminarsApp').directive('fileModel',
         }
     };
 }]);
-angular.module('seminarsApp').service('fileUpload',
+angular.module('seminarsApp').service('backupService',
 	['$http', function ($http) {
     this.uploadFileToUrl = function(file, url){
+       console.log("Sending " + file.name);
        var fd = new FormData();
        fd.append('file', file);
     
@@ -32,16 +33,30 @@ angular.module('seminarsApp').service('fileUpload',
        function(response){
     	   console.log('Failure with ' + response.status);
        });
-    }
+    };
 }]);
 angular.module('seminarsApp').controller('BackupController',
-	['$scope', 'fileUpload', function($scope, fileUpload) {
+	['$scope', 'backupService', function($scope, backupService) {
 	$scope.file = "no file chosen";
 	$scope.url = "http://localhost:8080/api/backup";
 	$scope.uploadFile = function() {
         var file = $scope.file;
         var url = $scope.url;
         
-        fileUpload.uploadFileToUrl(file, url);
+        backupService.uploadFileToUrl(file, url);
     };
+    $scope.dateStampOnCheck = function() {
+		if ($scope.filename == undefined) {
+			$scope.filename = "seminars-backup";
+		}
+		var filename = $scope.filename.slice(0, ($scope.filename.lastIndexOf(".") - 1 >>> 0) + 1);
+		var d = new Date();
+		var datestamp = "-" + d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear();
+		if ($scope.dateStampOn) {
+			filename = filename + datestamp;
+		} else {
+			filename = filename.slice(0, filename.lastIndexOf(datestamp));
+		}
+		$scope.filename = filename + ".json";
+	};
 }]);
